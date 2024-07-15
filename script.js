15b78f7cfe9620b818fbf105088b4638bcbfdfd3
Leon Gabriel <g73346920@gmail.com>
2024-07-15 15:14:36 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  // Your code here
+
+  const form = document.getElementById('registration-form');
+  const feedback = document.getElementById('form-feedback');
+})
