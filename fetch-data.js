f6437ca62879c3af933ee09a7b7a5ab3cb374265
Leon Gabriel <g73346920@gmail.com>
2024-07-16 15:28:20 +0300

    updates modified changes

---
+++ fetch-data.js
@@ -0,0 +1,6 @@
+// Async function
+ async function fetchUserData(){
+   
+  // URL API endpoint from which you’ll fetch user data.
+   const apiUrl = 'https://jsonplaceholder.typicode.com/users'
+ }
